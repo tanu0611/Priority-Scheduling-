ad6effd6dc0131b6be0b1e7858b371acab5ea83a
tanu0611 <32664178+tanu0611@users.noreply.github.com>
2018-04-05 00:06:15 +0530

    Add files via upload

--- Priority_Scheduling.c
+++ Priority_Scheduling.c
@@ -6,9 +6,22 @@ struct process
 	int bt;int ubt;int start;int wait;int comp;int tat;
 	
 };
+void increasepri(struct process p[],int n)
+{
+	int i;
+	for(i=0;i<n;i++)
+	{
+		if(p[i].wait!=0&&p[i].wait%2==0)
+		{
+			if(p[i].pri>1)
+			p[i].pri-=1;
+		}
+	}
+}
 int main()
 {
-	int time,p,i,j,r,working=-1;
+	int time,p,i,j,r,working=-1,current;
+	float avgtat=0,avgwt=0;
 	printf("Enter number of processes\n");
 	scanf("%d",&p);
 	struct process pro[p+1];
@@ -20,6 +33,7 @@ int main()
 		scanf("%d",&pro[i].at);
 		scanf("%d",&pro[i].bt);
 		scanf("%d",&pro[i].pri);
+		pro[i].wait=0;
 		pro[i].start=-1;
 		pro[i].ubt=pro[i].bt;
 	}
@@ -38,18 +52,51 @@ int main()
 		{	
 			if(pro[i].at<=time && pro[i].ubt!=0 && pro[i].pri<pro[working].pri)
 			{
-				if(pro[i].start==-1)
-				pro[i].start=time;
+				
 				working=i;
+				if(i!=working)
+				pro[i].wait+=1;
 			}
 		}
-		printf("At time %d Process%d is running\n",time,pro[working].id);
-		pro[working].ubt-=1;
-		time=time+1;
-		if(pro[working].ubt==0)
+		if(pro[working].start==-1)
 		{
+			if(r!=p)
+			{
+			pro[working].start=time+2;
+			}
+			else
+			pro[working].start=time;
+		}
+	//	printf("At time %d Process%d is running\n",time,pro[working].id);
+		pro[working].ubt-=1;
+		current=working;
+		time=time+1;
+		increasepri(pro,p);
+		 if(pro[working].ubt==0)
+		{
+			pro[working].comp=time;
+			time+=2;
+			current=working;
+			increasepri(pro,p);
 			working=p;
 			r--;
 		}
+		if(r!=10&&current!=working)
+		{
+			time+=2;
+			current=working;
+		}
 	}
+	printf("\n\nProcess   AT   BT   CT   TAT   WT\n");
+	for(i=0;i<p;i++)
+	{
+		pro[i].tat=pro[i].comp-pro[i].at;
+		avgtat=avgtat+pro[i].tat;
+		pro[i].wait=pro[i].tat-pro[i].bt;
+		avgwt=avgwt+pro[i].wait;
+		printf("%7d   %2d   %2d   %2d   %2d   %2d\n",pro[i].id,pro[i].at,pro[i].bt,pro[i].comp,pro[i].tat,pro[i].wait);
+	}
+	avgwt=(avgwt*1.0)/(p*1.0);
+	avgtat=(avgtat*1.0)/(p*1.0);
+	printf("Average waiting time is %0.2f\nAverage TAT is %0.2f\n",avgwt,avgtat);
 }
